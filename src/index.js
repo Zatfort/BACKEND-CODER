1ec33d67c5c846802e926b5ff33601290f12bbc3
daniel gutierrez <daniel_gutierrezn15@hotmail.com>
2023-03-09 16:54:20 -0300

    handlebars en el proyecto

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
 import express from "express"
 import ProductRouter from "./router/product.routes.js"
 import CartRouter from "./router/carts.routes.js"
-
+import { engine } from "express-handlebars"
+import * as path from "path"
+import __dirname from "./utils.js"
+import ProductManager from "./controllers/ProductManager.js"
 
 
 const app = express()
 
 const PORT = 8080
+const product = new ProductManager()
 
 app.use(express.json())
 app.use(express.urlencoded({ entended: true }))
+
+// handlerbars
+//------------
+ app.engine("handlebars", engine())
+ app.set("view engine", "handlebars")
+ app.set("views", path.resolve(__dirname + "/views"))
+//static
+//------------
+app.use("/", express.static(__dirname + "/public"))
+
+app.get("/", async (req,res) => {
+    const allProducts = await product.getProducts()
+    res.render("home",{
+        title: "express Avanzado | Handlebars",
+        products  : allProducts
+    })
+
+})
+
 
 app.use("/api/products", ProductRouter)
 app.use("/api/cart", CartRouter)
